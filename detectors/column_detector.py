1eb0d855a71ff8011d1ea4d19177aaeb6e6f11b4
agung prasetya <prasetya.agung@gmail.com>
2025-05-12 06:12:28 +0700

    update: Update function ekstraksi kolom json

--- detectors/column_detector.py
+++ detectors/column_detector.py
@@ -1,6 +1,21 @@
+import json
+import os
+
 class ColumnDetector():
-    def ekstraksi_semua_kolom_json(file_json):
-        pass
+    def ekstraksi_semua_kolom_json(self, file_name_json):
+        base_dir = os.path.dirname(__file__)
+        json_path = os.path.join(base_dir, '..', 'databases/column_detector',file_name_json)
+
+        with open(json_path,'r') as file:
+            data = json.load(file)
+
+        set_kolom = set()
+        for tabel in data['entitas']:
+            for kolom in data['entitas'][tabel]:
+                set_kolom.add(kolom)
+
+        daftar_kolom = list(set_kolom)
+        return daftar_kolom
 
     def tokenisasi(kalimat_ternormalisasi):
         kumpulan_kata = kalimat_ternormalisasi.split()
@@ -26,3 +41,6 @@ class ColumnDetector():
         daftar_kolom_simmilarity = self.jaccard_coefficient(daftar_kata=daftar_kata, daftar_kolom=daftar_kolom)
         daftar_kolom_select = filter(daftar_kolom_simmilarity=daftar_kolom_simmilarity,threshold=0.55)
         return daftar_kolom_select
+
+detector = ColumnDetector()
+daftar_kolom = detector.ekstraksi_semua_kolom_json(file_name_json='inventori_1.json')
