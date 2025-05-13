7700a6b1324ebdb4ea325a57a73e4ddc12cf1041
agung-prasetya <agungprasetya.indonesia@gmail.com>
2025-05-13 13:59:38 +0700

    refactor: include relevant files

---
+++ detectors/sorting/testing.py
@@ -0,0 +1,11 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+from detector import *
+from evaluator import *
+
+filepath_dataset = str(Path(__file__).parent / 'dataset' / 'dataset.xlsx')
+folderpath_database = str(Path(__file__).parent / 'dataset')
+evaluator=SingleLabelEvaluator(filepath_dataset=filepath_dataset,folderpath_database=folderpath_database, detector=SortingDetector())
+evaluator.evaluate()
