80e3737798ac3b27218c566ac25be67c2bacf7b2
Muhammadafifzaenal <apipjainul27@gmail.com>
2025-05-19 22:08:25 +0700

    merubah file detector.py

--- detectors/table/detector.py
+++ detectors/table/detector.py
@@ -7,47 +7,58 @@ class TableDetector():
         with open(filepath_database_json, 'r') as file:
             self.database = json.load(file)
 
-    def ekstraksi_semua_kolom_json(self):
-        set_kolom = set()
-        for tabel in self.database['entitas']:
-            for kolom in self.database['entitas'][tabel]:
-                set_kolom.add(kolom)
+    def ekstraksi_semua_table_json(self):
+        set_tabel = set()
+        for table in self.database['entitas']:
+            for table in self.database['entitas'][tabel]:
+                set_table.add(table)
 
-        daftar_kolom = list(set_kolom)
-        return daftar_kolom
+        daftar_table = list(set_table)
+        return daftar_table
 
     def tokenisasi(self, kalimat_ternormalisasi):
-        #Tujuan: mengubah kalimat menjadi daftar kata. 
-        #Misalnya, kalimat "tambahkan data baru di produk", daftar kata nya adalah ['tambahkan','data','baru','di','produk']
-        #Gunakan function split()
-        #Tugas: tambahkan code untuk tokenisasi. Jangan lupa mengapus pass
-        pass
+         daftar_kata = kalimat_ternormalisasi.split()
+         return daftar_kata
 
-    def set_daftar_kolom_simmilarity(self, daftar_kata, daftar_kolom):
-        #Tujuan: memberikan nilai ke setiap kolom database. Nilai ini merepresentasikan nilai maksimal jaccard coefficient 
-        #Tugas: tambahkan code untuk set_daftar_kolom_simmilarity. Lihat di catatan tentang set_daftar_kolom_simmilarity (di googleclassroom).
-        #Jangan lupa mengapus pass
-        pass
+
+
+    def set_daftar_table_simmilarity(self, daftar_kata, daftar_table):
+         hasil = []
+         set_kata = set(daftar_kata)
+         for table in daftar_table:
+            
+            tokens_kolom = table.lower().replace('_', ' ').split()
+            set_table = set(tokens_table)
+
+            irisan = set_kata & set_table
+            gabungan = set_kata | set_table
+            skor = len(irisan) / len(gabungan) if gabungan else 0
+
+            hasil.append({
+                'nama_table': table,
+                'nilai_simmilarity': skor
+            })
+            return hasil
 
     def normalisasi(self,kalimat):
-        #Tujuan: menormalisasi kalimat. Normalisasi disini maksudnya :
-        # 1. menyeragamkan huruf menjadi lowercase, Misal: kalimat "Tambahkan data Produk" menjadi "tambahkan data produk"
-        # 2. menghapus karakter non abjad dan non spasi, Misal: kalimat "tambahkan data produk!" menjadi "tambahkan data produk" 
-        # 3. menjadikan dua spasi atau lebih menjadi 1 spasi. Misal: kalimat "tambahkan    data produk" menjadi "tambahkan data produk" 
-        #Tugas: tambahkan code untuk normalisasi. Jangan lupa mengapus pass
-        pass
+       
+        kalimat = kalimat.lower()
+        kalimat = re.sub(r'[^a-z\s]', '', kalimat)        
+        kalimat = re.sub(r'\s+', ' ', kalimat).strip()
+        return kalimat
+
 
     def filter(self, daftar_kolom_simmilarity, threshold):
-        daftar_kolom_select = []
-        for kolom in daftar_kolom_simmilarity:
-            if kolom['nilai_simmilarity']>threshold:
-                daftar_kolom_select.append(kolom['nama_kolom'])
-        return daftar_kolom_select
+        daftar_table_select = []
+        for table in daftar_table_simmilarity:
+            if table['nilai_simmilarity']>threshold:
+                daftar_table_select.append(table['nama_table'])
+        return daftar_table_select
 
     def detect(self, kalimat):
-        daftar_kolom = self.ekstraksi_semua_kolom_json(file_name_json=self.file_database_json)
+        daftar_table = self.ekstraksi_semua_table_json(file_name_json=self.file_database_json)
         kalimat_ternormalisasi = self.normalisasi(kalimat=kalimat)
         daftar_kata = self.tokenisasi(kalimat_ternormalisasi=kalimat_ternormalisasi)
-        daftar_kolom_simmilarity = self.set_daftar_kolom_simmilarity(daftar_kata=daftar_kata, daftar_kolom=daftar_kolom)
-        daftar_kolom_select = self.filter(daftar_kolom_simmilarity=daftar_kolom_simmilarity,threshold=0.55)
-        return daftar_kolom_select
+        daftar_table_simmilarity = self.set_daftar_table_simmilarity(daftar_kata=daftar_kata, daftar_table=daftar_table)
+        daftar_table_select = self.filter(daftar_table_simmilarity=daftar_table_simmilarity,threshold=0.55)
+        return daftar_table_select
