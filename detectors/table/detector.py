1ac15a9042af6b8d01faa2bbccd7ba8a1d7213b1
agung-prasetya <agungprasetya.indonesia@gmail.com>
2025-05-21 12:13:39 +0700

    fix: fixing rule and dataset

--- detectors/table/detector.py
+++ detectors/table/detector.py
@@ -2,20 +2,16 @@ import json
 import re
 
 class TableDetector():
-    def __init__(self, filepath_database_json):
-        super().__init__()
-        self.filepath_database_json = filepath_database_json
-        with open(filepath_database_json, 'r') as file:
-            self.database = json.load(file)
 
-    def ekstraksi_semua_table_json(self):
+    def ekstraksi_semua_table_json(self, database):
         set_tabel = set()
-        for table in self.database['entitas']:
-            for table in self.database['entitas'][table]:
-                set_table.add(table)
-
-        daftar_table = list(set_table)
+        for table in database['entitas']:
+            set_tabel.add(table)
+            
+        daftar_table = list(set_tabel)
         return daftar_table
+
+
 
     def tokenisasi(self, kalimat_ternormalisasi):
          daftar_kata = kalimat_ternormalisasi.split()
@@ -23,23 +19,6 @@ class TableDetector():
 
 
 
-    def set_daftar_table_simmilarity(self, daftar_kata, daftar_table):
-         hasil = []
-         set_kata = set(daftar_kata)
-         for table in daftar_table:
-            
-            tokens_kolom = table.lower().replace('_', ' ').split()
-            set_table = set(token_table)
-
-            irisan = set_kata & set_table
-            gabungan = set_kata | set_table
-            skor = len(irisan) / len(gabungan) if gabungan else 0
-
-            hasil.append({
-                'nama_table': table,
-                'nilai_simmilarity': skor
-            })
-            return hasil
 
     def normalisasi(self,kalimat):
        
@@ -49,30 +28,52 @@ class TableDetector():
         return kalimat
 
 
-    def filter(self, daftar_kolom_simmilarity, threshold):
+
+
+    def filter(self, daftar_table_simmilarity, threshold):
         daftar_table_select = []
         for table in daftar_table_simmilarity:
             if table['nilai_simmilarity']>threshold:
                 daftar_table_select.append(table['nama_table'])
         return daftar_table_select
 
-    def detect(self, kalimat):
-        daftar_table = self.ekstraksi_semua_table_json(file_name_json=self.file_database_json)
+
+
+
+    def detect(self, kalimat, filepath_database_json):
+        with open(filepath_database_json, 'r') as file:
+            database = json.load(file)
+            
+            
+        daftar_table = self.ekstraksi_semua_table_json(database)
         kalimat_ternormalisasi = self.normalisasi(kalimat=kalimat)
         daftar_kata = self.tokenisasi(kalimat_ternormalisasi=kalimat_ternormalisasi)
         daftar_table_simmilarity = self.set_daftar_table_simmilarity(daftar_kata=daftar_kata, daftar_table=daftar_table)
-        daftar_table_select = self.filter(daftar_table_simmilarity=daftar_table_simmilarity,threshold=0.55)
+        daftar_table_select = self.filter(daftar_table_simmilarity=daftar_table_simmilarity,threshold=0.7)
+        
         return daftar_table_select
-    def set_daftar_table_simmilarity(daftar_table, daftar_kata):
-    daftar_table_simmilarity = []
+    
+    
+    
+    
+    def set_daftar_table_simmilarity(self, daftar_table, daftar_kata):
+        daftar_table_simmilarity = []
 
-    for table in daftar_table:
-        max_nilai = 0
-        for kata in daftar_kata:
-            nilai_jaccard_coefficient = jaccard_coefficient(table, kata)
-            if nilai_jaccard_coefficient > max_nilai:
-                max_nilai = nilai_jaccard_coefficient
-        data = {'nama_table': table, 'nilai_simmilarity': max_nilai}
-        daftar_table_simmilarity.append(data)
+        for table in daftar_table:
+            max_nilai = 0
+            for kata in daftar_kata:
+                nilai_jaccard_coefficient = self.jaccard_coefficient(table, kata)
+                if nilai_jaccard_coefficient > max_nilai:
+                    max_nilai = nilai_jaccard_coefficient
+            data = {'nama_table': table, 'nilai_simmilarity': max_nilai}
+            daftar_table_simmilarity.append(data)
 
-        return daftar_table_simmilarity
+            return daftar_table_simmilarity
+
+
+    
+    def jaccard_coefficient(self, kolom, kata):
+        irisan = set(kata) & set(kolom)
+        gabungan = set(kata) | set(kolom)
+        skor = len(irisan) / len(gabungan) if gabungan else 0
+        return skor
--- detectors/table/testing.py
+++ detectors/table/testing.py
@@ -4,3 +4,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '.
 
 from detector import *
 from evaluator import *
+
+filepath_dataset = str(Path(__file__).parent / 'dataset' / 'dataset.xlsx')
+folderpath_database = str(Path(__file__).parent / 'dataset')
+detector = TableDetector()
+evaluator=MultiLabelEvaluator(filepath_dataset=filepath_dataset,folderpath_database=folderpath_database, detector=detector)
+evaluator.evaluate()
